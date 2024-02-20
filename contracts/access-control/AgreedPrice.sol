c79c38ee63422091bf719099b668b7bf22bdc323
Yanel PHINERA-HORTH <yphinerahorth@gmail.com>
2024-02-20 10:47:14 +0100

    Access-control

--- contracts/access-control/AgreedPrice.sol
+++ contracts/access-control/AgreedPrice.sol
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AgreedPrice {
-  address public owner;
+contract AgreedPrice is Ownable{
   uint256 public price;
 
   constructor(uint256 _price) {
-    owner = msg.sender;
     price = _price;
     
   }
 
-  function updatePrice(uint256 _price) external  {
-    require(msg.sender == owner, "Restricted Access");
+  function updatePrice(uint256 _price) external onlyOwner {
     price = _price;
   }
 }
